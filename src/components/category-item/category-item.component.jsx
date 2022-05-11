cd8615c955f16642d2dc2c5544e5b42d83cad25b
TruongQuyHai <87081336+TruongQuyHai@users.noreply.github.com>
2022-05-11 10:13:53 +0700

    Build category list and category item

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+const CategoryItem = (props) => {
+  const { id, imageUrl, title } = props.category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
